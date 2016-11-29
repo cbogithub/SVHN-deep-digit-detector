e338d8502b8368ffcbb6d86b18a8ad86ba39f65e
joonsup <joonsup@joonsup-PC>
2016-11-29 17:31:51 +0900

    temp

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ import cv2
 
 img = cv2.imread("Samples//1.png")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-clone = img.copy()
 mser = cv2.MSER(_delta = 1)
-regions = mser.detect(gray, None)
+regions = mser.detect(img, None)
 
 print len(regions)
 
 # loop over the contours
 for region in regions:
     # fit a bounding box to the contour
+    clone = img.copy()
     (x, y, w, h) = cv2.boundingRect(region.reshape(-1,1,2))
     cv2.rectangle(clone, (x, y), (x + w, y + h), (0, 255, 0), 1)
- 
-cv2.imshow('img', clone)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('img', clone)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
